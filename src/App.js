d178524d7e9cfe5b8ab140475277a65f228233db
Padmaraj1998 <payanraj@gmail.com>
2025-02-23 13:08:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 function App() {
   return (
     <div >
-      <BrowserRouter>
+      <BrowserRouter basename="/blog">
       <Routes>
       <Route path="/*" element={<Home />} />
       </Routes>
